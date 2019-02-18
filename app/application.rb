6164f8f9a47eaad6724e9ac8c0905d6f4af37380
Seann Branchfield <eselbeus@gmail.com>
2019-02-18 01:34:08 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
       item_name = req.path.split("/items/").last 
       item = @@items.find do |i| 
         if i.name
-        i.name == item_name
+          i.name == item_name
         else
           resp.write "Item not found"
           resp.status 400
