d71307be17b293494f38aff665503c7c976412c0
Seann Branchfield <eselbeus@gmail.com>
2019-02-18 01:32:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,10 @@ class Application
     if req.path.match(/items/)
       
       item_name = req.path.split("/items/").last 
-      item = @@items.find{|i| i.name == item_name}
+      item = @@items.find do |i| 
+        i.name == item_name
+        
+      end
       binding.pry
       if item.nil?
         resp.write "Item not found"
