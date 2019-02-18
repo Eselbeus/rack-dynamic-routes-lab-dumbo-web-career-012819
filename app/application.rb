67c73c414b7dcd5fa50ce17c4aa759b345e4b0d7
Seann Branchfield <eselbeus@gmail.com>
2019-02-18 01:00:21 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,27 @@
+require 'pry'
+class Application 
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    
+    # if req.path.match(/items/)
+      
+      item_name = req.path.split("/items/").last 
+      # binding.pry
+      item = Item.all.find{|i| i.name == item_name}
+      binding.pry
+      if req.path == "/items/#{item}"
+        resp.write item.price
+      else
+        resp.write "Route not found"
+        resp.status = 404
+      end
+    # end
+      
+    resp.finish
+    
+  end
+  
+  
+end
