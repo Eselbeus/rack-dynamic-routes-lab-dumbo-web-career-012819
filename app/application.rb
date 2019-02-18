d461308303187ce667eb9e5e7080f91f3f661081
Seann Branchfield <eselbeus@gmail.com>
2019-02-18 01:43:36 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,6 @@ class Application
       item = @@items.find do |i| 
           i.name == item_name
       end
-      # binding.pry
       if item == nil
         resp.write "Item not found"
         resp.status = 400
